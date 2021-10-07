23db98011dec117812b73578967a9d78432b1207
Santosh1125 <santoshkumarp1126@gmail.com>
2021-10-07 20:36:29 +0530

    Folder Structure

---
+++ src/org/sample/Demo.java
@@ -0,0 +1,11 @@
+package org.sample;
+
+public class Demo {
+	
+	
+	public static void main(String[] args) {
+		Sample s = new Sample();
+		s.sample();
+}
+	
+}
---
+++ src/org/sample/Sample.java
@@ -0,0 +1,14 @@
+package org.sample;
+
+public class Sample {
+
+	protected void sample() {
+		System.out.println("Hello");
+	}
+	
+	public static void main(String[] args) {
+		
+		Sample s = new Sample();
+		s.sample();
+	}
+}
